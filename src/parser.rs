c377f6bee40077914005c59fa5b695fb022c9ba6
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2023-03-29 22:10:41 +0900

    Refactor: very beautiful

--- src/parser.rs
+++ src/parser.rs
@@ -26,44 +26,44 @@ impl<'a> State<'a> {
     pub const fn is_empty(&self) -> bool {
         self.tokens.is_empty()
     }
+
     pub const fn new(tokens: &'a [Token]) -> Self {
         Self { tokens }
     }
-    pub fn parse_primary_noun(&mut self) -> Result<PrimaryNoun, ParseError> {
+
+    pub fn next(&mut self) -> Result<Token, ParseError> {
         match self.tokens {
             [] => Err(ParseError::EndOfFile),
-            [Token::NormalIdent { ident }, ..] => {
+            [tok, ..] => {
                 self.tokens = &self.tokens[1..];
-                Ok(PrimaryNoun::Ident {
-                    ident: ident.clone(),
-                })
+                Ok(tok.clone())
             }
-            [Token::StringLiteral { literal }, ..] => {
-                self.tokens = &self.tokens[1..];
-                Ok(PrimaryNoun::StringLiteral {
-                    literal: literal.clone(),
-                })
-            }
-            [tok, ..] => Err(ParseError::UnexpectedToken {
+        }
+    }
+
+    ///
+    /// `primary_noun = ident | "<" character* ">"` 
+    #[allow(clippy::match_wildcard_for_single_variants)]
+    pub fn parse_primary_noun(&mut self) -> Result<PrimaryNoun, ParseError> {
+        let next = self.next()?;
+        match next {
+            Token::NormalIdent { ident } => Ok(PrimaryNoun::Ident { ident }),
+            Token::StringLiteral { literal } => Ok(PrimaryNoun::StringLiteral { literal }),
+            _ => Err(ParseError::UnexpectedToken {
                 expected: "（識別子か文字列リテラル）".to_string(),
-                actual: tok.clone(),
+                actual: next.clone(),
             }),
         }
     }
+
+    ///
+    /// `noun = (primary_noun "'d")* primary_noun`
     pub fn parse_noun(&mut self) -> Result<Noun, ParseError> {
         let mut pns = vec![];
-        let pn = self.parse_primary_noun()?;
-        pns.push(pn);
-        loop {
-            let ((), tokens2) = match self.tokens {
-                [Token::Reserved(Reserved::ApostropheD), ..] => Ok(((), &self.tokens[1..])),
-                _ => break,
-            }?;
-
-            self.tokens = tokens2;
-
-            let pn = self.parse_primary_noun()?;
-            pns.push(pn);
+        pns.push(self.parse_primary_noun()?);
+        while let [Token::Reserved(Reserved::ApostropheD), ..] = self.tokens {
+            self.tokens = &self.tokens[1..];
+            pns.push(self.parse_primary_noun()?);
         }
         let head = pns.pop().unwrap();
         Ok(Noun {
